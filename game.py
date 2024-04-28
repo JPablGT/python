b29b32a3b9ccba6eab5700cb8b9d35ee951efc24
Juan Pablo Gonzalez Trejos <136568787+JPablGT@users.noreply.github.com>
2024-04-27 19:40:49 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,21 @@
+import pygame
+from setttings import Settings
+from ship import Ship
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+    (ai_settings.screen_width, ai_settings.screen_height))  
+    pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
+
+
+    while True:
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        pygame.display.flip()
+
+        
+run_game()
