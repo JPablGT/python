a01fc04f5feb19a2a3325b55b0fc163d68868776
Juan Pablo Gonzalez Trejos <136568787+JPablGT@users.noreply.github.com>
2024-04-28 18:08:28 -0500

    mmm

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-    (ai_settings.screen_width, ai_settings.screen_height))  
+    (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     ship = Ship(screen)
@@ -19,7 +19,6 @@ def run_game():
         gf.update_screen(ai_settings, screen, ship)
         screen.fill(ai_settings.bg_color)
         ship.blitme()
-       
         
         
 run_game()
